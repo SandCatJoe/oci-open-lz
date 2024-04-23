f2e1a53f44009716256476911a6c6c9361cf7aad
SandCatJoe <rob.eke@hotmail.co.uk>
2024-04-23 14:17:32 +0100

    Test Build 1

---
+++ git-login.sh
@@ -0,0 +1,5 @@
+git config --global user.email "rob.eke@hotmail.co.uk"
+git config --global user.name "SandCatJoe"
+
+gh auth login
+
---
+++ git-push-changes.sh
@@ -0,0 +1,7 @@
+cd /home/openlz/oci-open-lz
+
+git add .
+git commit -m "Test Build 1"
+git branch -M main
+git push -u origin main
+
